ef2f482b334b4269fea52d3ccb77e9fc30eef8eb
kdpark <kdpark1623@gmail.com>
2020-06-15 17:35:42 +0900

    쓰지않는 주석과 TODO 제거

--- src/com/daou/logpjt/Main.java
+++ src/com/daou/logpjt/Main.java
@@ -1,11 +1,7 @@
 package com.daou.logpjt;
 
-/*
- * ���� Ŭ����
- */
 public class Main {
 	public static void main(String [] args){
-		//TODO �ð��� �����ɸ� Ʃ�� �ʿ���...
 		ClientExecute.execute();
 	}
 }
