0aee87d87df8606926c0013d13cb89f8f26207c6
kdpark <kdpark1623@gmail.com>
2020-06-15 17:36:10 +0900

    입력 받을떄 라인 단위로 입력받도록 변경

--- src/com/daou/logpjt/view/LongTimeReqView.java
+++ src/com/daou/logpjt/view/LongTimeReqView.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 import com.daou.logpjt.model.LogDataModel;
 import com.daou.logpjt.util.LogTimeUtil;
 
-public class LongTimeReqView extends AbstractView {
+public class LongTimeReqView {
 	public LogDataModel show() {
 		Scanner s = new Scanner(System.in);
 		LogDataModel model = new LogDataModel("LongTimeReqController/run");
 		System.out.println("시작 시간을 입력해주세요. : (ex 18/May/2020:00:01:29)");
-		String start = s.next();
+		String start = s.nextLine();
 		model.addAttribute("startTime", LogTimeUtil.getUnixTime(start));
 		System.out.println("끝나는 시간을 입력해주세요.  : (ex 18/May/2020:11:11:30)");
-		String end = s.next();
+		String end = s.nextLine();
 		model.addAttribute("endTime", LogTimeUtil.getUnixTime(end));
 		System.out.println("응답에 걸린 시간을 입력해주세요 : (ex 1,2,3...)");
 		double resTime = s.nextDouble();
 		model.addAttribute("resTime",resTime);
 		System.out.println(start+" - "+end+" 사이의  "+resTime+"이상 걸린 request API: ");
+		
+		s.close();
 		return model;
 	}
 }
