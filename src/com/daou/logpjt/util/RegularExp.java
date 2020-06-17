c11f071f63c60c8c2c88363a6c4f80eac70b27f1
kdpark <kdpark1623@gmail.com>
2020-06-17 16:56:35 +0900

    각 로그enum tags 정규식 등록

--- src/com/daou/logpjt/util/RegularExp.java
+++ src/com/daou/logpjt/util/RegularExp.java
@@ -2,12 +2,16 @@ package com.daou.logpjt.util;
 
 
 public enum RegularExp {
-	LOGTIME("(?<LOGTIME>\\S)"),
-	METHOD("\\S"),
-	RESCODE("\\S"),
-	DELAY("\\S"),
-	REQAPI("\\S"),
-	DEVICE("\\S");
+	LOGTIME("\\[(?<LOGTIME>.+)\\]"),
+	METHOD(" (?<METHOD>\\D{1,7}?) \\/"),
+	RESCODE("HTTP.{1,4} (?<RESCODE>\\d{3})"),
+	DELAY(" (?<DELAY>\\d?\\.\\d{3,})"),
+	REQAPI("\\w (?<REQAPI>\\/api((?!\\/\\w+@|\\/\\%|\\/\\w+\\.\\w+|\\/\\w+\\-|\\/\\d|\\?|\\=|[A-Z])\\S){1,})"),
+	DEVICE("(?<DEVICE>\"Mozilla\\/5\\.0 .+?\\\")"),
+	CHROME("Chrome"),
+	WINDOW(" MSIE 8\\.0"),
+	FIREFOX("Firefox"),
+	SAFARI(" Version/5.0.2 Safari/");
 //	OPERA();
 	private String regex;
 	
